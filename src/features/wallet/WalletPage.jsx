53f17b407daccc331322148f8b89e15820755e82
Muhammad Ibaadi Ilmi <ibaadijaya2@gmail.com>
2026-03-06 20:01:03 +0700

    Implement basic wallet home page

--- src/features/wallet/WalletPage.jsx
+++ src/features/wallet/WalletPage.jsx
@@ -1,13 +1,39 @@
 // Wallet feature pages
-// Person in charge: [Nama anggota 4]
+// Person in charge: Badi
 // Backend repo: Wallet
+
+function MarkTopUpSuccess() {
+  return (
+    <div>
+      <h1>Mark top-up request as successful</h1>
+      <form>
+        <label htmlFor="request-id">Top-up request id:</label>
+        <input type='number' id='request-id' required /><br />
+        <input type='submit' />
+      </form>
+    </div>
+  )
+}
+
+function MarkTopUpFailed() {
+  return (
+    <div>
+      <h1>Mark top-up request as failed</h1>
+      <form>
+        <label htmlFor="request-id">Top-up request id:</label>
+        <input type='number' id='request-id' required /><br />
+        <input type='submit' />
+      </form>
+    </div>
+  )
+}
 
 function WalletPage() {
   return (
-    <div className="wallet-page">
-      <h1>My Wallet</h1>
-      <p>TODO: Implement wallet balance & transactions</p>
-    </div>
+      <nav>
+        <MarkTopUpSuccess />
+        <MarkTopUpFailed />
+      </nav>
   );
 }
 
